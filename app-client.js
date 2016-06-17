5a3e565e8fa728840fcb2968ed3f8c6cf726f770
Jared Cain <jared.cain77@gmail.com>
2016-06-16 17:32:00 -0700

    Add carousel rough draft

--- app-client.js
+++ app-client.js
@@ -1,7 +1,11 @@
 window.jQuery = $ = require("jquery");
 require("jquery-ui");
 require("bootstrap");
+// require('./src/js/utils');
+require('./src/js/custCarousel');
+
 
 $(document).ready(function(){
     console.log("Loaded");
-})
+
+});
---
+++ src/js/custCarousel.js
@@ -0,0 +1,29 @@
+window.jQuery = $ = require('jquery');
+// require('npm-modernizr');
+// require('./utils');
+
+module.export = (function () {
+
+    var carouselInit = function() {
+        var carousel = document.getElementById('carousel'),
+            navButtons = document.querySelectorAll('#carousel-navigation button'),
+            panelCount = carousel.children.length,
+            theta = 0,
+            // var tz = Math.round( ( panelSize / 2 ) /
+            //         Math.tan( Math.PI / numberOfPanels ) ),
+
+            onNavButtonClick = function(e) {
+                var increment = parseInt(event.target.getAttribute("data-increment"));
+                theta += ( 360 / panelCount ) * increment * -1;
+                carousel.style['transform'] = `translateZ(-577px) rotateY(${theta}deg)`;
+                console.log("im clicked");
+            };
+
+        for(var i = 0; i < 2; i++) {
+            navButtons[i].addEventListener('click', onNavButtonClick, false);
+        }
+    };
+
+    window.addEventListener('DOMContentLoaded', carouselInit, false);
+
+})();
